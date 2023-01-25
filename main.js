9ffa7f8e86ed383b5b31edf7211635fd96cf852e
velklusia <116886960+velklusia@users.noreply.github.com>
2023-01-25 14:00:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-//Changes classes on on the icons of the items to show them when we get a value on the different keys.
-//https://www.folkstalk.com/tech/replace-class-with-code-examples/ here I got help with how to replace one class with another. 
+//The following code works because of the courtesy of the lab assistant Lucas that have helped me a lot to fix all the bugs that appeared in the written code. Thank you Lucas!
 
 let book=document.getElementById("book")
 let heels=document.getElementById("heels")
@@ -43,7 +42,7 @@ if(typeof (present)!="undefined"&&present!=null){
 
 function updateCollectedItems() {
     if(sessionStorage.getItem("pickedUpbook") !== null){
-        console.log("You managed to pick up the magic book! There you can try to find a solution to create a magic coctail to save smurphs before it is too late! you have party")
+        console.log("You managed to pick up the magic book! There you can try to find a solution to create a magic coctail to save smurphs before it is too late!")
         book.classList.replace("bookHidden", "bookVisible");
     }
     if(sessionStorage.getItem("pickedUpheels") !== null){
@@ -55,7 +54,7 @@ function updateCollectedItems() {
         present.classList.replace("presentHidden", "presentVisible");
     }
 }
-//Makes the browser load the following in the end of the HTML.
+
 window.onload = function() {
      let party = document.getElementById("party");
      let win = document.getElementById("win");
@@ -64,17 +63,14 @@ window.onload = function() {
     updateCollectedItems();
     let winRandom = Math.floor(Math.random() * 2);
     console.log(winRandom);
-    //Adds a key ("party") and a value ("true") in the sessionStorage and there by the updateCollectedItems to show the book icon on the top.
-    //if is in the beginning added to only use Listener if we have an id called "party". This to not get errors in the code.
+   
 if(party){
     party.addEventListener("click", () => {
         saveToSessionStorage("party", true);
         updateCollectedItems();
      });
 }
-    // Adds a key ("present") and a value ("true")" in the sessionStorage and there by the updateCollectedItems to show the present icon on the top.
-    // This if "party" has a value. If it doesn't, then you get an alert message instad that you need "party" first.
-    //if is in the beginning added to only use Listener if we have an id called "gargamel". This to not get errors in the code.
+
 if(gargamel){
     gargamel.addEventListener("click", function () {
         if (sessionStorage.getItem("party")){
@@ -86,9 +82,7 @@ if(gargamel){
         }
     });
 }
-    // Changes the value of "heels" to "true" in the sessionStorage and there by the updateCollectedItems to show the present icon on the top.
-    // This if "present" has a value. If it doesn't, then you get an alert message instad that you need "present" first.
-    //if is in the beginning added to only use Listener if we have an id called "village". This to not get errors in the code.
+   
 if(village){
     village.addEventListener("click", function () {
         if (sessionStorage.getItem("present")){ 
@@ -97,15 +91,13 @@ if(village){
             updateCollectedItems();
             } 
             else {
-                alert("Before getting into your village you need to get a present that is going to distract Gargamel!")
+                alert("Before heading to the village you need to get a present that is going to distract Gargamel!")
                 }
     });
 }
-    //if is in the beginning added to only use Listener if we have an id called "win". This to not get errors in the code.
+   
 if(win){
     win.addEventListener("click", function () {
-            //If mathRandom has the value 0 you get an alert message that you dropped your item. 
-            // If the value is 0 sessionStorage.clear is executed. 
         if (winRandom === 0){
             alert ("Oh no! Gargamel caught another Smurf while you were busy saving smurfette. Hurry up and collect your items again to save Brainy.")
             sessionStorage.clear();
@@ -114,21 +106,17 @@ if(win){
                item[i].style.display="none"
             }
         }
-            //If winRandom is another value than 0 and have a value on "party", "present" & "heels" you get an alert winning message. 
         else if (winRandom === 1){
             if (sessionStorage.getItem("pickedUpbook") && sessionStorage.getItem("pickedUppresent") && sessionStorage.getItem("pickedUpheels")){ 
-            alert("🏅 You got all the items that you needed to save a Smurfette! Amazing job! 🎉")
+            alert("You got all the things that you needed to save a Smurfette! Smurfs are now free and they are no longer endangered by Gargamel")
             }
             else{
-                alert("You have to get your heels, present and a book before leaving home!")
+                alert("You have to get your heels, present and a book before you can save all the Smurfs!")
                 }
         }
-
-            //If you don't have values on all three of these above, you get an alert that you have to get them first.
     });
 }
 };
-//Function that enabled me to store everything that a person picked up in the session storage. That means that after refreshing the page I am still able as a player to see the items I gathered before
 
 function saveToSessionStorage(key, value) {
     sessionStorage.setItem(key, value);
